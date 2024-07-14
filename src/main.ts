94dba956d18971d86de888765735d2b8bc455ad2
kitamura-hitomi-911 <hinitial@gmail.com>
2024-07-14 20:02:04 +0900

    grapesjs & MJML 取り込み

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import 'grapesjs/dist/css/grapes.min.css';
+import grapesjs from 'grapesjs'
+import grapesJSMJML from 'grapesjs-mjml'
+import './style.css'
+
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
+
+console.log(grapesJSMJML)
+const editor = grapesjs.init({
+  container : '#gjs',
+  plugins:[grapesJSMJML],
+  pluginsOpts: {
+    [grapesJSMJML as any]: {
+      useCustomTheme:false
+    }
+  },
+  storageManager: false,
+  components: `<mjml>
+    <mj-body>
+      <!-- Your MJML body here -->
+      <mj-section>
+        <mj-column>
+          <mj-text>My Company</mj-text>
+        </mj-column>
+      </mj-section>
+    </mj-body>
+  </mjml>`,
+  style: `'.txt-red{color: red}'`,
+});
+console.log(editor)
+/* 
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!) */
