a8c2546172afde6ca85f138d96380bcb205b7ba7
kitamura-hitomi-911 <hinitial@gmail.com>
2024-10-14 11:23:17 +0900

    見た目調整中

--- src/tiptapEditor.ts
+++ src/tiptapEditor.ts
@@ -1,66 +1,156 @@
-import { LitElement, html } from 'lit';
-import { customElement/* , property */ } from 'lit/decorators.js';
-import mainStyles from './TiptapEditor.css?inline'
-import remixicon from 'remixicon/fonts/remixicon.css?inline'
+import { LitElement, html } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+import mainStyles from "./TiptapEditor.css?inline";
+import remixicon from "remixicon/fonts/remixicon.css?inline";
 
-@customElement('tiptap-editor')
-export class TiptapEditor extends LitElement {      
-
+@customElement("tiptap-editor")
+export class TiptapEditor extends LitElement {
   createRenderRoot() {
     return this; // Light DOMにする
   }
 
-  onBtnClick= (e:Event) => {
-    e.preventDefault()
-    console.log(e)
-    const tgtElm = e.currentTarget as HTMLButtonElement
-    const action = tgtElm.dataset.action || ''
-    if(action){
-        const event = new CustomEvent('on-click-tiptap-editor', {
-            detail: { action },
-            bubbles: true
-            });
-        
-            // カスタムイベントを発火
-            this.dispatchEvent(event);
+  @property()
+  hoge: boolean = true;
+
+  @state()
+  actionList: (
+    | {
+        name: string;
+        param?: string;
+        label: string;
+        icon?: string;
+        ui: "btn_icon" | "btn_label" | "select";
+      }
+    | "separator"
+  )[] = [
+    {
+      name: "textAlign",
+      param: "left",
+      label: "左揃え",
+      icon: "ri-align-left",
+      ui: "btn_icon",
+    },
+    {
+      name: "textAlign",
+      param: "center",
+      label: "中央揃え",
+      icon: "ri-align-center",
+      ui: "btn_icon",
+    },
+    {
+      name: "textAlign",
+      param: "right",
+      label: "右揃え",
+      icon: "ri-align-center",
+      ui: "btn_icon",
+    },
+    "separator",
+    {
+      name: "bold",
+      label: "太字",
+      icon: "ri-bold",
+      ui: "btn_icon",
+    },
+    {
+      name: "italic",
+      label: "斜体",
+      icon: "ri-italic",
+      ui: "btn_icon",
+    },
+    {
+      name: "underline",
+      label: "下線",
+      icon: "ri-underline",
+      ui: "btn_icon",
+    },
+    "separator",
+    {
+      name: "bulletList",
+      label: "箇条書き",
+      icon: "ri-list-unordered",
+      ui: "btn_icon",
+    },
+    {
+      name: "orderList",
+      label: "ナンバリング",
+      icon: "ri-list-ordered",
+      ui: "btn_icon",
+    },
+    "separator",
+    {
+      name: "link",
+      label: "リンク",
+      icon: "ri-list-ordered",
+      ui: "btn_icon",
+    },
+    {
+      name: "h1",
+      label: "見出し",
+      ui: "select",
+    },
+    {
+      name: "mergetag",
+      label: "差し込みタグ",
+      ui: "btn_label",
+    },
+  ];
+
+  onBtnClick = (e: Event) => {
+    e.preventDefault();
+    console.log(e);
+    const tgtElm = e.currentTarget as HTMLButtonElement;
+    const action = tgtElm.dataset.action || "";
+    if (action) {
+      const event = new CustomEvent("on-click-tiptap-editor", {
+        detail: { action },
+        bubbles: true,
+      });
+
+      // カスタムイベントを発火
+      this.dispatchEvent(event);
     }
-  }
+  };
 
   render() {
-    return html`<style>${mainStyles}</style><style>${remixicon}</style>
-    <div class="wrapper">
-        <button data-action="textAlign" data-param="left" @click="${this.onBtnClick}" class="is-icon"><i class="ri-align-left"></i></button>
-        <button data-action="textAlign" data-param="center" @click="${this.onBtnClick}" class="is-icon"><i class="ri-align-center"></i></button>
-        <button data-action="textAlign" data-param="right" @click="${this.onBtnClick}" class="is-icon"><i class="ri-align-right"></i></button>
-        <span class="separator"></span>
-        <button data-action="bold" @click="${this.onBtnClick}" class="is-icon"><i class="ri-bold"></i></button>
-        <button data-action="italic" @click="${this.onBtnClick}" class="is-icon"><i class="ri-italic"></i></button>
-        <button data-action="underline" @click="${this.onBtnClick}" class="is-icon"><i class="ri-underline"></i></button>
-        <span class="separator"></span>
-        <button data-action="bulletList" @click="${this.onBtnClick}" class="is-icon"><i class="ri-list-unordered"></i></button>
-        <button data-action="orderList" @click="${this.onBtnClick}" class="is-icon"><i class="ri-list-ordered"></i></button>
-        <span class="separator"></span>
-        <button data-action="h1" @click="${this.onBtnClick}" class="is-icon">H1</button>
-        <button data-action="h2" @click="${this.onBtnClick}" class="is-icon">H2</button>
-        <button data-action="h3" @click="${this.onBtnClick}" class="is-icon">H3</button>
-        <button data-action="color" @click="${this.onBtnClick}">赤</button>
-        <button data-action="color" @click="${this.onBtnClick}">青</button>
-        <button data-action="color" @click="${this.onBtnClick}">黄色</button>
-        <button data-action="color" @click="${this.onBtnClick}">黒</button>
-        <button data-action="color" @click="${this.onBtnClick}">差し込みタグ</button>
-        <button data-action="link" @click="${this.onBtnClick}" class="is-icon">Link</button>
-
-        
-        </div>
-    `;
+    return html`<style>
+        ${mainStyles}</style
+      ><style>
+        ${remixicon}
+      </style>
+      <div class="wrapper">
+        ${this.actionList.map((action) =>
+          action === "separator"
+            ? html`<span class="separator"></span>`
+            : html`<button
+                data-action="${action.name}"
+                data-param="${action.param}"
+                @click="${this.onBtnClick}"
+                class="${classMap({
+                  "is-icon": action.ui === "btn_icon",
+                  "is-label": action.ui === "btn_label",
+                  "is-select": action.ui === "select",
+                })}"
+              >
+                ${action.icon
+                  ? html`<i class="${action.icon}"></i>`
+                  : html`<span class="label">${action.label}</span>`}
+                ${action.ui === "select"
+                  ? html`<span class="dropdown"
+                      ><i class="ri-arrow-down-s-line"></i
+                    ></span>`
+                  : ""}
+              </button>`
+        )}
+      </div> `;
   }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'tiptap-editor': TiptapEditor;
+    "tiptap-editor": TiptapEditor;
   }
   interface HTMLElementEventMap {
-    'on-click-tiptap-editor': CustomEvent;
+    "on-click-tiptap-editor": CustomEvent;
   }
 }
